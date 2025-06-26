1da655a8ab606194578b6cb74f6c1507bd3b22ab
blagoja <blagojabozinovski@gmail.com>
2025-06-26 16:36:52 +0200

    Customise the styling

--- ckanext/chat_widget/assets/js/chat_widget.js
+++ ckanext/chat_widget/assets/js/chat_widget.js
@@ -1,4 +1,4 @@
-  function toggleChat() {
-    const chatBox = document.getElementById('chatBox');
-    chatBox.style.display = chatBox.style.display === 'flex' ? 'none' : 'flex';
-  }
+function toggleChat() {
+  const chatBox = document.getElementById('chatBox');
+  chatBox.style.display = chatBox.style.display === 'flex' ? 'none' : 'flex';
+}
