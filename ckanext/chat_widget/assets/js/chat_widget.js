9cf8f45ae35a65d398233b5a856846e3fb004d5f
blagoja <blagojabozinovski@gmail.com>
2025-06-26 17:17:50 +0200

    Style update and restrict the widget for logged in users

--- ckanext/chat_widget/assets/js/chat_widget.js
+++ ckanext/chat_widget/assets/js/chat_widget.js
@@ -2,3 +2,10 @@ function toggleChat() {
   const chatBox = document.getElementById('chatBox');
   chatBox.style.display = chatBox.style.display === 'flex' ? 'none' : 'flex';
 }
+
+if (window !== window.top) {
+    const chatButton = document.querySelector('.chat-button');
+    if (chatButton) {
+        chatButton.style.display = 'none';
+    }
+}
